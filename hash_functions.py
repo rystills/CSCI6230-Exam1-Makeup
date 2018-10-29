75db1ce1de3cdbfcc67c34059c0b2fdd6828cf08
Ryan Stillings <rystills@gmail.com>
2018-10-29 03:21:46 -0400

    split into interweaved and end inserted modes

--- hash_functions.py
+++ hash_functions.py
@@ -14,30 +14,13 @@ def base_convert(i, b,n):
             i = i // b
     return [0]*(n-len(result)) + result
 
-def main():
+def endInsert(originalMessage,fraudMessage):
     start_time = time.time()
-    #1,3. establish legitimate and fradulent base messages
-    originalMessage = input("please enter legitimate message (leave blank for default): ")
-    if (originalMessage == ""):
-        originalMessage = "The quick brown fox jumps over the lazy dog."
-    print("legitimate message detected as '{0}'".format(originalMessage))
-    fraudMessage = input("please enter fraudulent message (leave blank for default): ")
-    if (fraudMessage == ""):
-        fraudMessage = "The scheming purple fox jumps onto the frightened dog."
-    print("fraudulent message detected as '{0}'".format(fraudMessage))
-    
     #2. B generates 2^m/2 variations x' of x 
     print("generating x' variations of legitimate message")
     xp = [0]*numVariations
-    #xpo = [0]*numVariations
     for i in range(numVariations):
-        print(base_convert(i, 4,8))
-    #digits = '0123'
-    #for num in itertools.product(digits,repeat=4):
-        #s = ''.join(num)
-        #print(int(s, 4))
-        #xpo[i] = originalMessage + " \b"*i
-        #xp[i] = hashlib.md5((xpo[i]).encode("utf-8")).hexdigest()[:hashChars]
+        xp[i] = hashlib.md5((originalMessage + " \b"*i).encode("utf-8")).hexdigest()[:hashChars]
     print("finished generating {0} variations of original message in time = {1} seconds".format(numVariations,int(time.time() - start_time)))
     start_time = time.time()
     
@@ -49,9 +32,23 @@ def main():
         if hashedFraudVar in xp:
             ind = xp.index(hashedFraudVar)
             print("found y' number {0} = '{1} with hash {2} matching \nx' number {3} = '{4} in time = {5} seconds".format(
-                i,fraudMessage + "'" + ' + "\\032\\008"*'+str(i),hashedFraudVar,ind,
-                originalMessage + "'" + ' + "\\032\\008"*'+str(ind),int(time.time() - start_time)))
+                i,fraudMessage + "'" + ' + " \\b"*'+str(i),hashedFraudVar,ind,
+                originalMessage + "'" + ' + " \\b"*'+str(ind),int(time.time() - start_time)))
             break
+
+def main():
+    #1,3. establish legitimate and fradulent base messages
+    originalMessage = input("please enter legitimate message (leave blank for default): ")
+    if (originalMessage == ""):
+        originalMessage = "The quick brown fox jumps over the lazy dog."
+    print("legitimate message detected as '{0}'".format(originalMessage))
+    fraudMessage = input("please enter fraudulent message (leave blank for default): ")
+    if (fraudMessage == ""):
+        fraudMessage = "The scheming purple fox jumps onto the frightened dog."
+    print("fraudulent message detected as '{0}'".format(fraudMessage))
+    if (len(fraudMessage.split(" ")) < 9 or len(originalMessage.split(" ")) < 9):
+        print("too few spaces to properly interweave insertions; sticking space-backspace characters at the end instead")
+        return endInsert(originalMessage,fraudMessage)
 
 if __name__ == "__main__":
     main()
